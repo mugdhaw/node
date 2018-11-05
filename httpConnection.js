43d9252156c658e5f531ee2182fbfeb610444862
mugdha <mugdha.wadhokar@gmail.com>
2018-11-05 15:20:26 +0530

    Handled Http Web Server Request and Response

--- httpConnection.js
+++ httpConnection.js
@@ -1,6 +1,23 @@
-var http = require('http');
+var http = require('http'); // Import Node.js core module
 
-http.createServer( function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('This is first node program. Hello World!');
-}).listen(8080);
+var server = http.createServer( function(req, res){ // create web server
+    console.log("request url ::",req.url);
+    if (req.url == "/") { // check the URL of the current request
+        // set response header
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        // set response Content
+        res.write("This is home page");
+		res.end();
+    } else if(req.url == "/student") { 
+        res.writeHead(200, {'Content-Type': 'text/html'});
+         res.write('<html><body><p>This is student Page.</p></body></html>');
+		res.end();
+    } else if (req.url == '/data') { // To serve JSON response
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify({ message: "Hello, This is Json Response"}));  
+            res.end();  
+    } else {
+         res.end('Invalid Request!');
+    }
+});
+server.listen(5050);
