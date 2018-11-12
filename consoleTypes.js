fa8f989f9655a293433213e2756b42cba471cd55
mugdha <mugdha.wadhokar@gmail.com>
2018-11-12 14:28:28 +0530

    Console Types supported by node js

---
+++ consoleTypes.js
@@ -0,0 +1,13 @@
+// Simple console log
+console.log('Hello %s', 'JavaTpoint');
+
+// console.error() function is used to render error message on console
+console.error(new Error('This is a wrong method.'));
+
+// console.warn() function is used to display warning message on console.
+const name = 'Mugdha';
+console.warn('This is warning message, Dont mess with ${name}');
+
+// ` is a back quote not a single quote
+const name1 = 'John';
+console.warn(`Don't mess with me ${name1}! Don't mess with me!`);
